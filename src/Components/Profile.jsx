a9cee4ed316d8d1af82cfbdd032a91f4b4a4fdab
Marin <marin.muktic3@gmail.com>
2023-07-06 14:51:38 +0200

    image handler

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -3,30 +3,43 @@ import React, { useState } from "react";
 const Profile = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
+  const handleImageChange = (event) => {
+    setSelectedImage(event.target.files[0]);
+  };
+
   return (
     <>
       <section className="bg-white text-black text-left m-2 w-full rounded-lg p-4 profile">
-        {selectedImage && (
-          <div>
+        <div className="border-solid border-2 border-blue-500 w-32 h-32 relative rounded-lg flex justify-center items-center">
+          {selectedImage && (
             <img
+              className="rounded-lg absolute w-full h-full"
               alt="not found"
-              width={"250px"}
               src={URL.createObjectURL(selectedImage)}
             />
-            <button onClick={() => setSelectedImage(null)}>Remove</button>
-          </div>
-        )}
-        <label htmlFor="files">Select</label>
-        <input
-          id="files"
-          className="hidden"
-          type="file"
-          name="myImage"
-          onChange={(event) => {
-            console.log(event.target.files[0]);
-            setSelectedImage(event.target.files[0]);
-          }}
-        />
+          )}
+          <label
+            htmlFor="files"
+            className="bg-black text-white rounded-lg p-1 absolute cursor-pointer opacity-75"
+          >
+            {selectedImage ? "Change Image" : "Select Image"}
+          </label>
+          {selectedImage && (
+            <button
+              onClick={() => setSelectedImage(null)}
+              className="bg-red-500 text-white p-0.5 rounded-lg absolute bottom-2 right-2"
+            >
+              Remove
+            </button>
+          )}
+          <input
+            id="files"
+            className="hidden"
+            type="file"
+            name="myImage"
+            onChange={handleImageChange}
+          />
+        </div>
       </section>
     </>
   );
