74a0eb245324ff61880d9b8bee7c26098ba7060c
Marin <marin.muktic3@gmail.com>
2023-07-06 10:57:13 +0200

    image upload

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <>
       <section className="bg-white text-black text-left m-2 w-full rounded-lg p-4 profile">
-        New Div
+        {selectedImage && (
+          <div>
+            <img
+              alt="not found"
+              width={"250px"}
+              src={URL.createObjectURL(selectedImage)}
+            />
+            <button onClick={() => setSelectedImage(null)}>Remove</button>
+          </div>
+        )}
+        <label htmlFor="files">Select</label>
+        <input
+          id="files"
+          className="hidden"
+          type="file"
+          name="myImage"
+          onChange={(event) => {
+            console.log(event.target.files[0]);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
       </section>
     </>
   );
