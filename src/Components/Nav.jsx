6613509464d60f9e183b33e3813bab5362efbc96
Marin <marin.muktic3@gmail.com>
2023-07-14 11:42:07 +0200

    separate folder for icons

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfileImg from "../Icons/Profile"
+import PaperClipImg from "../Icons/PaperClip"
+import LinkImg from "../Icons/Link"
+import PreviewImg from "../Icons/Preview"
+
 
 const Nav = ({ activeSection, onSectionChange }) => {
   return (
     <div className="bg-white flex items-center justify-between p-3">
       <div className="flex">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"
-          />
-        </svg>
+        <PaperClipImg />
         <h1 className="hidden sm:block">
           Link sharing App
         </h1>
@@ -30,20 +22,7 @@ const Nav = ({ activeSection, onSectionChange }) => {
         >
           <span className="hidden sm:block">Links</span>
           <span className="sm:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-              />
-            </svg>
+             <LinkImg/>
           </span>
         </button>
         <button
@@ -54,20 +33,7 @@ const Nav = ({ activeSection, onSectionChange }) => {
         >
           <span className="hidden sm:block">Profile Details</span>
           <span className="sm:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
+            <ProfileImg/>
           </span>
         </button>
       </div>
@@ -75,25 +41,7 @@ const Nav = ({ activeSection, onSectionChange }) => {
         <Link to="/preview">
           <span className="hidden sm:block">Preview</span>
           <span className="sm:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
+            <PreviewImg />
           </span>
         </Link>
       </button>
